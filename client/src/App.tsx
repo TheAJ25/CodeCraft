5fe425f3b74515c4ef1b4cd204f11550e68647c3
Abhijit Shaw <abhijitshaw2501@gmail.com>
2024-07-11 13:47:33 +0530

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button.tsx"
+function App() {
+  return <>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam similique quis omnis atque, pariatur quibusdam, maiores neque nobis eligendi aut nihil mollitia perspiciatis sunt quod rem dicta.
+
+  <Button>Button</Button>
+  </>;
+}
+
+export default App;
